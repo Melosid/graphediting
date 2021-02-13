bd4d66ba6490f04b5c6733ed280cf0a533d886b5
melos <melosdauti7@gmail.com>
2021-02-13 19:37:27 +0100

    sidemenu options

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useRef } from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css'
+import uploadIcon from './icons/upload-thick.svg'
+import downloadIcon from './icons/download-thick.svg'
+import plusSquare from './icons/plusSquare.svg'
 
-function App() {
+const App = () => {
+  const fileSectionRef = useRef()
+
+  const handleClick = () => {
+    const section = fileSectionRef.current
+    section.classList.toggle('closed')
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className="graph"></div>
+      <div className="options">
+        <text className="header">Graph Editing</text>
+        <text className="description">Add a graph to edit and visualize</text>
+        <div className="sections">
+          <text className="fileSectionToggle" onClick={handleClick}>File options</text>
+          <div ref={fileSectionRef} className="fileSection">
+            <div className="uploadGr">
+              <img className="uploadIcon" src={uploadIcon} />
+              <text className="uploadText">Upload a .gr graph file</text>
+            </div>
+            <div className="addArff">
+              <img className="plusIcon" src={plusSquare} />
+              <text className="uploadText">Add .arff file to graph</text>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
-}
+};
 
 export default App;

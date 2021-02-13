5ad05332611a056bc04c21313851f6c7d4c00096
melos <melosdauti7@gmail.com>
2021-02-13 22:13:01 +0100

    graph options

--- src/App.js
+++ src/App.js
@@ -5,16 +5,29 @@ import uploadIcon from './icons/upload-thick.svg'
 import downloadIcon from './icons/download-thick.svg'
 import plusSquare from './icons/plusSquare.svg'
 import downMenu from './icons/downMenu.svg'
-import upMenu from './icons/upMenu.svg'
+import addNode from './icons/addNode.svg'
+import links from './icons/links.svg'
+import modify from './icons/modify.svg'
+import removeNode from './icons/removeNode.svg'
+
 
 const App = () => {
   const fileSectionRef = useRef()
   const fileSectionMenuIconRef = useRef()
+  const graphSectionRef = useRef()
+  const graphSectionMenuIconRef = useRef()
 
-  const handleClick = () => {
+  const toggleFileSection = () => {
     const section = fileSectionRef.current
     section.classList.toggle('closed')
     const icon = fileSectionMenuIconRef.current
+    icon.classList.toggle('rotate')
+  }
+
+  const toggleGraphSection = () => {
+    const section = graphSectionRef.current
+    section.classList.toggle('closed')
+    const icon = graphSectionMenuIconRef.current
     icon.classList.toggle('rotate')
   }
 
@@ -25,11 +38,11 @@ const App = () => {
         <text className="header">Graph Editing</text>
         <text className="description">Add a graph to edit and visualize</text>
         <div className="sections">
-          <div className="fileSectionToggle" onClick={handleClick}>
-            <text className="fileSectionTitle">File options</text>
+          <div className="sectionToggle" onClick={toggleFileSection}>
+            <text className="sectionTitle">File options</text>
             <img ref={fileSectionMenuIconRef} className="menuIcon" src={downMenu} />
           </div>
-          <div ref={fileSectionRef} className="fileSection closed">
+          <div ref={fileSectionRef} className="fileSection">
             <div className="uploadGr">
               <img className="uploadIcon" src={uploadIcon} />
               <text className="uploadText">Upload a .gr graph file</text>
@@ -37,6 +50,37 @@ const App = () => {
             <div className="addArff">
               <img className="plusIcon" src={plusSquare} />
               <text className="uploadText">Add .arff file to graph</text>
+            </div>
+          </div>
+          <div className="sectionToggle" onClick={toggleGraphSection}>
+            <text className="sectionTitle">Graph options</text>
+            <img ref={graphSectionMenuIconRef} className="menuIcon" src={downMenu} />
+          </div>
+          <div ref={graphSectionRef} className="fileSection">
+            <div className="addArff">
+              <img className="plusIcon" src={addNode} />
+              <text className="uploadText">Add node</text>
+            </div>
+            <div className="modifyLink">
+              <div>
+                <img className="plusIcon" src={links} />
+                <text className="uploadText">Modify links</text>
+              </div>
+              <text className="nodeText">Start:</text>
+              <input className="nodeInput" placeholder="1" />
+              <img className="modifyIcon" src={modify} />
+              <text className="nodeText2">End:</text>
+              <input className="nodeInput" placeholder="2" />
+              <text className="modifyLinkButton">Modify</text>
+            </div>
+            <div className="modifyLink">
+              <div>
+                <img className="plusIcon" src={removeNode} />
+                <text className="uploadText">Remove node</text>
+              </div>
+              <text className="nodeText">Node:</text>
+              <input className="nodeInput" placeholder="1" />
+              <text className="removeNodeButton">Remove</text>
             </div>
           </div>
         </div>

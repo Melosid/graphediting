c30d122b4222c6b6e1a9b795ff059a89942fef18
melos <melosdauti7@gmail.com>
2021-02-20 14:53:19 +0100

    code cleaning and effects

--- src/App.js
+++ src/App.js
@@ -1,35 +1,17 @@
 import React, { useState, useRef, useEffect } from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css'
-import uploadIcon from './icons/upload-thick.svg'
-import plusSquare from './icons/plusSquare.svg'
-import downMenu from './icons/downMenu.svg'
-import addNode from './icons/addNode.svg'
-import links from './icons/links.svg'
-import modify from './icons/modify.svg'
-import removeNode from './icons/removeNode.svg'
-import csvDoc from './icons/csvFile.svg'
-import outputDoc from './icons/outputFile.svg'
-import { ForceGraph } from './components/forceGraph'
+import { SidebarMenu } from './components/sidebarMenu'
+import { ForceGraph } from './components/d3Components/forceGraph'
 import { graph1, graph2 } from "./data/data2";
 import { initialMatrix, finalMatrix, differenceMatrix } from './components/matrixGenerators'
-import { graphGenerator } from './components/graphGenerator'
+import { graphGenerator } from './components/d3Components/graphGenerator'
 import { downloadFile } from './components/downloadFile'
 
 const App = () => {
-  const fileSectionRef = useRef()
-  const fileSectionMenuIconRef = useRef()
-  const graphSectionRef = useRef()
-  const graphSectionMenuIconRef = useRef()
-  const downloadSectionRef = useRef()
-  const downloadSectionMenuIconRef = useRef()
-
   const reader = new FileReader();
 
   const [grGraphFile, setGrGraphFile] = useState()
-  const uploadGrGraphFile = () => {
-    document.getElementById("grUpload").click()
-  }
   const handleGrFileUpload = (ev) => {
     reader.readAsText(ev.target.files[0], "utf-8");
     reader.onload = (event) => {
@@ -41,11 +23,7 @@ const App = () => {
     };
   };
 
-
   const [arffGraphFile, setArffGraphFile] = useState()
-  const uploadArffGraphFile = () => {
-    document.getElementById("arffUpload").click()
-  }
   const handleArffFileUpload = (ev) => {
     reader.readAsText(ev.target.files[0], "utf-8");
     reader.onload = (event) => {
@@ -56,11 +34,19 @@ const App = () => {
   };
 
   const handleCsvDownload = () => {
-    downloadFile("Graph", csvFile, "toCSV.csv");
+    if (csvFile) {
+      downloadFile("Graph", csvFile, "toCSV.csv");
+    } else {
+      window.alert('No .gr graph file found. Please upload a graph before proceeding.')
+    }
   };
 
   const handleOutFileDownload = () => {
-    downloadFile("Graph", outputFile, "OutputFormat.txt");
+    if (outputFile) {
+      downloadFile("Graph", outputFile, "OutputFormat.txt");
+    } else {
+      window.alert('Missing .gr or .arff graph file. Please upload graph files before proceeding.')
+    }
   };
 
   const [graph, setGraph] = useState(graph1)
@@ -89,27 +75,6 @@ const App = () => {
     }
   }, [diffMatrix])
 
-  const toggleFileSection = () => {
-    const section = fileSectionRef.current
-    section.classList.toggle('closed')
-    const icon = fileSectionMenuIconRef.current
-    icon.classList.toggle('rotate')
-  }
-
-  const toggleGraphSection = () => {
-    const section = graphSectionRef.current
-    section.classList.toggle('closed')
-    const icon = graphSectionMenuIconRef.current
-    icon.classList.toggle('rotate')
-  }
-
-  const toggleDownloadSection = () => {
-    const section = downloadSectionRef.current
-    section.classList.toggle('closed')
-    const icon = downloadSectionMenuIconRef.current
-    icon.classList.toggle('rotate')
-  }
-
   const nodeHoverTooltip = React.useCallback((node) => {
     return `<div>     
       <b>${node.name}</b>
@@ -121,84 +86,14 @@ const App = () => {
       <div className="graph">
         <ForceGraph nodeHoverTooltip={nodeHoverTooltip} graph={graph} />
       </div>
-      <div className="options">
-        <span className="header">Graph Editing</span>
-        <span className="description">Add a graph to edit and visualize</span>
-        <div className="sections">
-          <div className="sectionToggle" onClick={toggleFileSection}>
-            <span className="sectionTitle">File options</span>
-            <img ref={fileSectionMenuIconRef} className="menuIcon" src={downMenu} />
-
-          </div>
-          <div ref={fileSectionRef} className="fileSection">
-            <div className="uploadGr" onClick={uploadGrGraphFile}>
-              <img className="uploadIcon" src={uploadIcon} />
-              <span className="uploadspan">Upload a .gr graph file</span>
-              <input
-                id="grUpload"
-                type="file"
-                onChange={handleGrFileUpload}
-                className="noDisplay"
-              />
-            </div>
-            <div className="addArff" onClick={uploadArffGraphFile}>
-              <img className="plusIcon" src={plusSquare} />
-              <span className="uploadspan">Add .arff file to graph</span>
-              <input
-                id="arffUpload"
-                type="file"
-                onChange={handleArffFileUpload}
-                className="noDisplay"
-              />
-            </div>
-          </div>
-          <div className="sectionToggle" onClick={toggleGraphSection}>
-            <span className="sectionTitle">Graph options</span>
-            <img ref={graphSectionMenuIconRef} className="menuIcon" src={downMenu} />
-          </div>
-          <div ref={graphSectionRef} className="fileSection">
-            <div className="addArff">
-              <img className="plusIcon" src={addNode} />
-              <span className="uploadspan">Add node</span>
-            </div>
-            <div className="modifyLink">
-              <div>
-                <img className="plusIcon" src={links} />
-                <span className="uploadspan">Modify links</span>
-              </div>
-              <span className="nodespan">Start:</span>
-              <input className="nodeInput" placeholder="1" />
-              <img className="modifyIcon" src={modify} />
-              <span className="nodespan2">End:</span>
-              <input className="nodeInput" placeholder="2" />
-              <span className="modifyLinkButton">Modify</span>
-            </div>
-            <div className="modifyLink">
-              <div className="removeNodeTitle">
-                <img className="plusIcon" src={removeNode} />
-                <span className="uploadspan">Remove node</span>
-              </div>
-              <span className="nodespan">Node:</span>
-              <input className="nodeInput" placeholder="1" />
-              <span className="removeNodeButton">Remove</span>
-            </div>
-          </div>
-          <div className="sectionToggle" onClick={toggleDownloadSection}>
-            <span className="sectionTitle">Download</span>
-            <img ref={downloadSectionMenuIconRef} className="menuIcon" src={downMenu} />
-          </div>
-          <div ref={downloadSectionRef} className="fileSection">
-            <div className="uploadGr" onClick={handleCsvDownload}>
-              <img className="uploadIcon" src={csvDoc} />
-              <span className="uploadspan">Download CSV file</span>
-            </div>
-            <div className="addArff" onClick={handleOutFileDownload}>
-              <img className="plusIcon" src={outputDoc} />
-              <span className="uploadspan">Download Output file</span>
-            </div>
-          </div>
-        </div>
-      </div>
+      <SidebarMenu
+        grGraphFile={grGraphFile}
+        arffGraphFile={arffGraphFile}
+        handleGrFileUpload={handleGrFileUpload}
+        handleArffFileUpload={handleArffFileUpload}
+        handleCsvDownload={handleCsvDownload}
+        handleOutFileDownload={handleOutFileDownload}
+      />
     </div>
   );
 };
---
+++ src/components/sidebarMenu.js
@@ -0,0 +1,138 @@
+import React, { useRef } from 'react'
+import '../App.css'
+import redo from '../icons/redoIcon.svg'
+import uploadIcon from '../icons/upload-thick.svg'
+import plusSquare from '../icons/plusSquare.svg'
+import downMenu from '../icons/downMenu.svg'
+import addNode from '../icons/addNode.svg'
+import links from '../icons/links.svg'
+import modify from '../icons/modify.svg'
+import removeNode from '../icons/removeNode.svg'
+import csvDoc from '../icons/csvFile.svg'
+import outputDoc from '../icons/outputFile.svg'
+
+export const SidebarMenu = (props) => {
+    const fileSectionRef = useRef()
+    const fileSectionMenuIconRef = useRef()
+    const graphSectionRef = useRef()
+    const graphSectionMenuIconRef = useRef()
+    const downloadSectionRef = useRef()
+    const downloadSectionMenuIconRef = useRef()
+
+    const uploadGrGraphFile = () => {
+        document.getElementById("grUpload").click()
+    }
+
+    const uploadArffGraphFile = () => {
+        if (props.grGraphFile) {
+            document.getElementById("arffUpload").click()
+        }
+        else {
+            window.alert('No .gr graph file found. Please upload a graph before proceeding.')
+        }
+    }
+
+    const toggleFileSection = () => {
+        const section = fileSectionRef.current
+        section.classList.toggle('closed')
+        const icon = fileSectionMenuIconRef.current
+        icon.classList.toggle('rotate')
+    }
+
+    const toggleGraphSection = () => {
+        const section = graphSectionRef.current
+        section.classList.toggle('closed')
+        const icon = graphSectionMenuIconRef.current
+        icon.classList.toggle('rotate')
+    }
+
+    const toggleDownloadSection = () => {
+        const section = downloadSectionRef.current
+        section.classList.toggle('closed')
+        const icon = downloadSectionMenuIconRef.current
+        icon.classList.toggle('rotate')
+    }
+
+    return (<div className="options">
+        <span className="header">Graph Editing</span>
+        <span className="description">Add a graph to edit and visualize</span>
+        <div className="reset">
+            <img className="resetIcon" src={redo} />
+            <span className="resetText">Reset options</span>
+        </div>
+        <div className="sections">
+            <div className="sectionToggle" onClick={toggleFileSection}>
+                <span className="sectionTitle">File options</span>
+                <img ref={fileSectionMenuIconRef} className="menuIcon" src={downMenu} />
+            </div>
+            <div ref={fileSectionRef} className="fileSection">
+                <div className={props.grGraphFile ? 'uploadGr green' : 'uploadGr'} onClick={uploadGrGraphFile}>
+                    <img className="uploadIcon" src={uploadIcon} />
+                    <span className="uploadspan">Upload a .gr graph file</span>
+                    <input
+                        id="grUpload"
+                        type="file"
+                        onChange={props.handleGrFileUpload}
+                        className="noDisplay"
+                    />
+                </div>
+                <div className={props.arffGraphFile ? 'addArff green' : 'addArff'} onClick={uploadArffGraphFile}>
+                    <img className="plusIcon" src={plusSquare} />
+                    <span className="uploadspan">Add .arff file to graph</span>
+                    <input
+                        id="arffUpload"
+                        type="file"
+                        onChange={props.handleArffFileUpload}
+                        className="noDisplay"
+                    />
+                </div>
+            </div>
+            <div className="sectionToggle" onClick={toggleGraphSection}>
+                <span className="sectionTitle">Graph options</span>
+                <img ref={graphSectionMenuIconRef} className="menuIcon" src={downMenu} />
+            </div>
+            <div ref={graphSectionRef} className="fileSection">
+                <div className="addArff">
+                    <img className="plusIcon" src={addNode} />
+                    <span className="uploadspan">Add node</span>
+                </div>
+                <div className="modifyLink">
+                    <div>
+                        <img className="plusIcon" src={links} />
+                        <span className="uploadspan">Modify links</span>
+                    </div>
+                    <span className="nodespan">Start:</span>
+                    <input className="nodeInput" placeholder="1" />
+                    <img className="modifyIcon" src={modify} />
+                    <span className="nodespan2">End:</span>
+                    <input className="nodeInput" placeholder="2" />
+                    <span className="modifyLinkButton">Modify</span>
+                </div>
+                <div className="modifyLink">
+                    <div className="removeNodeTitle">
+                        <img className="plusIcon" src={removeNode} />
+                        <span className="uploadspan">Remove node</span>
+                    </div>
+                    <span className="nodespan">Node:</span>
+                    <input className="nodeInput" placeholder="1" />
+                    <span className="removeNodeButton">Remove</span>
+                </div>
+            </div>
+            <div className="sectionToggle" onClick={toggleDownloadSection}>
+                <span className="sectionTitle">Download</span>
+                <img ref={downloadSectionMenuIconRef} className="menuIcon" src={downMenu} />
+            </div>
+            <div ref={downloadSectionRef} className="fileSection">
+                <div className={props.grGraphFile ? 'uploadGr green' : 'uploadGr'} onClick={props.handleCsvDownload}>
+                    <img className="uploadIcon" src={csvDoc} />
+                    <span className="uploadspan">Download CSV file</span>
+                </div>
+                <div className={props.arffGraphFile ? 'addArff green' : 'addArff'} onClick={props.handleOutFileDownload}>
+                    <img className="plusIcon" src={outputDoc} />
+                    <span className="uploadspan">Download Output file</span>
+                </div>
+            </div>
+        </div>
+    </div>)
+
+}
